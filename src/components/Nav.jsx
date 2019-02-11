0b875b7fec4bb8932df9567fd0a520e543a43d99
kmchen <kmchen@kmchens-MacBook-Pro.local>
2019-02-11 08:37:05 +0100

    React boilertemplate with webpack and scss

---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export const Nav = () =>
+  <nav className="navbar has-shadow">
+    <div className="container">
+      <div className="navbar-brand"><a className="navbar-item" href="../">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/GYG_Expressive_Logo_01_Multi_CMYK_HR_%281%29.jpg/1200px-GYG_Expressive_Logo_01_Multi_CMYK_HR_%281%29.jpg"/></a>
+      </div>
+    </div>
+  </nav>;
